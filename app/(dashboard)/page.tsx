9436ea4e3569903a2f1e309573d5d2815f21bc37
Safo Emmanuel <nanasafo122@gmail.com>
2024-02-14 00:15:50 +0000

    second commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+const Dashboard = () => {
+  return ( 
+    <div>
+      Dashboard
+    </div>
+  );
+}
+ 
+export default Dashboard;
